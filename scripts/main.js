64d4f2cb5b1981dc8fdf2c6fbe1a755e96f2579b
Jason Allen <jasonallen@Jasons-MacBook-Pro.local>
2018-06-04 07:57:41 -0500

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,93 @@
+var Colors = JSON.parse('[{ "color1": "red", "color2": "#0F338F","color3": "yellow","color4": "#57595C","color5": "#D0D4DB"},{"color1": "red", "color2": "#0018A1","color3": "yellow","color4": "#2A315C","color5": "#345FDB"},{"color1": "red", "color2": "#64BFDB","color3": "yellow","color4": "#087A00","color5": "#1519C7"},{"color1": "red", "color2": "#087A00","color3": "yellow","color4": "#FF6500","color5": "#FFF1E5"},{"color1": "#FFF1E5", "color2": "#1E34C7","color3": "yellow","color4": "#789ED1","color5": "#9AADC7"},{"color1": "red", "color2": "#087A00","color3": "yellow","color4": "#D19F78","color5": "#D0D4DB"},{"color1": "#E03900", "color2": "#0F338F","color3": "yellow","color4": "#E09E7A","color5": "#474746"},{"color1": "#087A00", "color2": "#733200","color3": "yellow","color4": "#57595C","color5": "#C4926A"}]');
+
+
+$("#metalMan").click(function(){
+	// var ColorsArray = Colors[Math.random(Math.floor() * Colors.length)];
+	$("#box1").css("background-color", Colors[0].color1);
+	$("#box2").css("background-color", Colors[0].color2);
+	$("#box3").css("background-color", Colors[0].color3);
+	$("#box4").css("background-color", Colors[0].color4);
+	$("#box5").css("background-color", Colors[0].color5);
+	$("#robotMaster").text("Metal Man");
+	
+});
+
+$("#airMan").click(function(){
+	// var ColorsArray = Colors[Math.random(Math.floor() * Colors.length)];
+	$("#box1").css("background-color", Colors[1].color1);
+	$("#box2").css("background-color", Colors[1].color2);
+	$("#box3").css("background-color", Colors[1].color3);
+	$("#box4").css("background-color", Colors[1].color4);
+	$("#box5").css("background-color", Colors[1].color5);
+	$("#robotMaster").text("Air Man");
+	
+});
+
+$("#bubbleMan").click(function(){
+	// var ColorsArray = Colors[Math.random(Math.floor() * Colors.length)];
+	$("#box1").css("background-color", Colors[2].color1);
+	$("#box2").css("background-color", Colors[2].color2);
+	$("#box3").css("background-color", Colors[2].color3);
+	$("#box4").css("background-color", Colors[2].color4);
+	$("#box5").css("background-color", Colors[2].color5);
+	$("#robotMaster").text("Bubble Man");
+});
+
+$("#crashMan").click(function(){
+	// var ColorsArray = Colors[Math.random(Math.floor() * Colors.length)];
+	$("#box1").css("background-color", Colors[3].color1);
+	$("#box2").css("background-color", Colors[3].color2);
+	$("#box3").css("background-color", Colors[3].color3);
+	$("#box4").css("background-color", Colors[3].color4);
+	$("#box5").css("background-color", Colors[3].color5);
+	$("#robotMaster").text("Crash Man");
+});
+
+$("#flashMan").click(function(){
+	// var ColorsArray = Colors[Math.random(Math.floor() * Colors.length)];
+	$("#box1").css("background-color", Colors[4].color1);
+	$("#box2").css("background-color", Colors[4].color2);
+	$("#box3").css("background-color", Colors[4].color3);
+	$("#box4").css("background-color", Colors[4].color4);
+	$("#box5").css("background-color", Colors[4].color5);
+	$("#robotMaster").text("Flash Man");
+});
+
+$("#heatMan").click(function(){
+	// var ColorsArray = Colors[Math.random(Math.floor() * Colors.length)];
+	$("#box1").css("background-color", Colors[5].color1);
+	$("#box2").css("background-color", Colors[5].color2);
+	$("#box3").css("background-color", Colors[5].color3);
+	$("#box4").css("background-color", Colors[5].color4);
+	$("#box5").css("background-color", Colors[5].color5);
+	$("#robotMaster").text("Heat Man");
+});
+
+$("#quickMan").click(function(){
+	// var ColorsArray = Colors[Math.random(Math.floor() * Colors.length)];
+	$("#box1").css("background-color", Colors[6].color1);
+	$("#box2").css("background-color", Colors[6].color2);
+	$("#box3").css("background-color", Colors[6].color3);
+	$("#box4").css("background-color", Colors[6].color4);
+	$("#box5").css("background-color", Colors[6].color5);
+	$("#robotMaster").text("Quick Man");
+});
+
+
+$("#woodMan").click(function(){
+	// var ColorsArray = Colors[Math.random(Math.floor() * Colors.length)];
+	$("#box1").css("background-color", Colors[7].color1);
+	$("#box2").css("background-color", Colors[7].color2);
+	$("#box3").css("background-color", Colors[7].color3);
+	$("#box4").css("background-color", Colors[7].color4);
+	$("#box5").css("background-color", Colors[7].color5);
+	$("#robotMaster").text("Wood Man");
+});
+
+
+
+
+
+
+
+
